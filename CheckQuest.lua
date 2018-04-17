89efd51472f1174a2863deb3da8d5d89188f7e7c
Jabberie <jabberie@gmail.com>
2018-04-17 15:17:28 +0100

    Update

--- CheckQuest.lua
+++ CheckQuest.lua
@@ -1,21 +1,44 @@
 
 --=====================================================================
+local MyScanningTooltip = CreateFrame("GameTooltip", "MyScanningTooltip", UIParent, "GameTooltipTemplate")
+
+local QuestTitleFromID = setmetatable({}, { __index = function(t, id)
+  MyScanningTooltip:SetOwner(UIParent, "ANCHOR_NONE")
+  MyScanningTooltip:SetHyperlink("quest:"..id)
+  local title = MyScanningTooltipTextLeft1:GetText()
+  MyScanningTooltip:Hide()
+  if title and title ~= RETRIEVING_DATA then
+    t[id] = title
+    return title
+  end
+end })
+
+
+
 local function MyAddonCommands(msg, editbox)
   myQuestID = string.match(msg,"%d+");
-  myResult = "Incomplete";
+  myResult = "is |cffff0000Incomplete|r on this character.";
   
   if (myQuestID == nil) or (myQuestID == '') then
     print("Please enter a Quest ID.")
   elseif myQuestID ~= '' then
     if (IsQuestFlaggedCompleted(myQuestID)) then
-          --myQuestName = GetQuestInfo(myQuestID)
-          myResult = "Completed";
+          --myQuestName = QuestTitleFromID(myQuestID)
+          myQuestName = QuestTitleFromID[myQuestI]D;
+          if myQuestName == nil then
+            myQuestName = QuestTitleFromID[myQuestI]D;
+          end
+          myResult = "has been |cFF00FF00Completed|r on this character.";
     end
-    print("QuestID: "..myQuestID.." : "..myResult);
+    print("CheckQuest: The quest "..myQuestName.."("..myQuestID..") "..myResult);
+    --print("CheckQuest: The quest ("..myQuestID..") "..myResult);
   else
     print("You broke it, somehow!");
   end
 end
+
+
+
 
 SLASH_CHECKQUEST1, SLASH_CHECKQUEST2 = '/checkquest', '/cq'
 SlashCmdList["CHECKQUEST"] = MyAddonCommands 
